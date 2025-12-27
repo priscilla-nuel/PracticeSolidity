477e70004ba27fa4610f442bd010ea917401a492
priscilla-nuel <prizzyllanwafor@gmail.com>
2025-12-27 19:20:17 +0100

    Add new Solidity contracts and update factory logic

---
+++ contracts/AddFiveStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.18;
+
+//Inheritance in solidity
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+// this line allows Addfive to inherit everything from SimpleStorage
+contract AddFiveStorage is SimpleStorage {
+  
+
+}
